c8e170cbe1ebcb770569c38a9bcd52e2a52bc643
JennyBortnouskaya <gienie.b17@gmail.com>
2026-03-08 01:11:53 +0300

    Text animation

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,45 +1,44 @@
+'use client';
+import { useEffect, useState } from 'react';
 import styles from './Hero.module.scss';
 import Image from 'next/image';
 import bg from '../../assets/main_bg.png';
 
 export default function Hero() {
+    const [offset, setOffset] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => setOffset(window.scrollY * 0.3);
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     return (
         <section className={styles.hero}>
-
-            {/* фон */}
             <Image
                 src={bg}
                 alt="Mountain background"
                 fill
                 priority
+                style={{ transform: `translateY(${offset}px)` }}
                 className={styles.bg}
             />
-
             <div className={styles.overlay} />
-
             <div className="container">
-
-                {/* Follow sidebar */}
                 <div className={styles.follow}>
                     <span>Follow us</span>
                 </div>
-
-                {/* Основной контент */}
                 <div className={styles.content}>
                     <span className={styles.label}>A Hiking Guide</span>
-
                     <h1>
                         Be Prepared For The <br />
                         Mountains And Beyond!
                     </h1>
-
                     <div className={styles.scroll}>
                         <span>scroll down</span>
                         <div className={styles.arrow}></div>
                     </div>
-
                 </div>
-
             </div>
         </section>
     );
