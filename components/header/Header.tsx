86a02a542a733806fdc22ed3146bf5f51431d6f4
JennyBortnouskaya <gienie.b17@gmail.com>
2026-03-08 17:13:15 +0300

    Added icon to account

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,15 +1,19 @@
 'use client'
 
 import styles from './Header.module.scss'
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 
 export default function Header() {
     return (
         <header className={styles.header}>
             <div className="container">
+
                 <div className={styles.inner}>
+
                     <div className={styles.logo}>
                         MNTN
                     </div>
+
                     <nav className={styles.nav}>
                         <a href="#">Equipment</a>
                         <a href="#">About us</a>
@@ -17,6 +21,7 @@ export default function Header() {
                     </nav>
 
                     <div className={styles.account}>
+                        <AccountCircleOutlinedIcon className={styles.icon} />
                         <span>Account</span>
                     </div>
 
