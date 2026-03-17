32e6810a8a69fa951c3926914349bb64834c7990
JennyBortnouskaya <gienie.b17@gmail.com>
2026-03-18 01:14:05 +0300

    Added font. adjusted 1 section

--- components/ContentSection/Section01.tsx
+++ components/ContentSection/Section01.tsx
@@ -7,7 +7,7 @@ export default function Section01() {
             number="01"
             label="GET STARTED"
             title="What level of hiker are you?"
-            text="Determining what level of hiker you are can be an important tool when planning future hikes. This hiking level guide will help you plan hikes according to different hike ratings."
+            text="Determining what level of hiker you are can be an important tool when planning future hikes. This hiking level guide will help you plan hikes according to different hike ratings set by various websites like All Trails and Modern Hiker. What type of hiker are you – novice, moderate, advanced moderate, expert, or expert backpacker? "
             image={img}
         />
     )
